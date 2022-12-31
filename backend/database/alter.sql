0668d6664f1235adbbc4f7d342216c8b3ac955fc
Antonio Avolio <antonioavolio18@gmail.com>
2022-12-31 11:51:29 +0100

    Fixed bug about mysql server
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- backend/database/alter.sql
+++ backend/database/alter.sql
@@ -1,9 +1,9 @@
---FOREIGN KEYS CONSTRAINTS
+-- FOREIGN KEYS CONSTRAINTS
 
 ALTER TABLE `INBOOK`.`FORNITORI`
 ADD CONSTRAINT fk_fornitori_utenti_visitatori FOREIGN KEY FORNITORI(ID_utente_fornitore) REFERENCES UTENTI_VISITATORI(ID)
 ON DELETE CASCADE;
---Aggiungere vincolo per stabilire che tutti i fornitori nella tabella FORNITORI abbiano un'istanza nella tabella UTENTI_VISITATORI con TIPO = FORNITORE (da fare con una funzione in mysql o da gestire in js)
+-- Aggiungere vincolo per stabilire che tutti i fornitori nella tabella FORNITORI abbiano un'istanza nella tabella UTENTI_VISITATORI con TIPO = FORNITORE (da fare con una funzione in mysql o da gestire in js)
 
 ALTER TABLE `INBOOK`.`ORARI_ATTIVITA`
 ADD CONSTRAINT fk_orari_fornitori FOREIGN KEY ORARI(ID_fornitore) REFERENCES FORNITORI(ID_utente_fornitore)
@@ -24,9 +24,9 @@ ADD CONSTRAINT fk_servizi_fornitori FOREIGN KEY SERVIZI(ID_fornitore) REFERENCES
 ON DELETE NO ACTION;
 
 
---DOMAIN CONSTRAINTS (i piu complicati vengono gestiti nella logica di business (js))
+-- DOMAIN CONSTRAINTS (i piu complicati vengono gestiti nella logica di business (js))
 
---vincolo su ORARI_ATTIVITA: orario apertura < chiusura, e su 0<orario<24
+-- vincolo su ORARI_ATTIVITA: orario apertura < chiusura, e su 0<orario<24
 ALTER TABLE `INBOOK`.`ORARI_ATTIVITA`
 ADD CONSTRAINT chk_ORARI_ATTIVITA_apertura CHECK (Orario_apertura>'00:00:00' and Orario_apertura<'23:59:59');
 ALTER TABLE `INBOOK`.`ORARI_ATTIVITA`
@@ -34,12 +34,12 @@ ADD CONSTRAINT chk_ORARI_ATTIVITA_chiusura CHECK (Orario_chiusura>'00:00:00' and
 ALTER TABLE `INBOOK`.`ORARI_ATTIVITA`
 ADD CONSTRAINT chk_ORARI_ATTIVITA_apertura_chiusura CHECK (Orario_apertura<Orario_chiusura);
 
---vincolo su PRENOTAZIONI: orario prenotazione - Orario_richiesta > 4h
+-- vincolo su PRENOTAZIONI: orario prenotazione - Orario_richiesta > 4h
 ALTER TABLE `INBOOK`.`PRENOTAZIONI`                                                     
 ADD CONSTRAINT chk_PRENOTAZIONI_orario_prenotazione_richiesta CHECK ((TIMEDIFF(Orario_prenotazione, Orario_richiesta)) > '4:00:00');
 
---questi vincoli possono essere gestiti solo con le funzioni/trigger, altrimenti in js
---capienza MAX rispetto alle prenotazioni
---vincolo sull'intervallo [orario apertura2,orario chiusura2] dev'essere disgiunto da [orario apertura1,orario apertura1]
---vincolo su utente che non può effettuare piu richieste di prenotazione 
---per uno stesso orario (o orari non disgiunti)
+-- questi vincoli possono essere gestiti solo con le funzioni/trigger, altrimenti in js
+-- capienza MAX rispetto alle prenotazioni
+-- vincolo sull'intervallo [orario apertura2,orario chiusura2] dev'essere disgiunto da [orario apertura1,orario apertura1]
+-- vincolo su utente che non può effettuare piu richieste di prenotazione 
+-- per uno stesso orario (o orari non disgiunti)
--- backend/database/create.sql
+++ backend/database/create.sql
@@ -1,48 +1,48 @@
-CREATE TABLE `INBOOK`.`UTENTI_VISITATORI`(
-    ID char(16) NOT NULL UNIQUE PRIMARY KEY,
-    Nome char(16) NOT NULL ,
-    Cognome char(16) NOT NULL ,
-    Email varchar(20) NOT NULL UNIQUE,
-    Password_acc varchar(20) NOT NULL,
-    Data_di_nascita date,
-    Telefono varchar(10) NOT NULL UNIQUE,
-    Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Tipo ENUM('Cliente', 'Amministratore', 'Fornitore')  --se NULL sono solo utente visitatore (ancora non ho completato la registrazione)
-);
+    CREATE TABLE `INBOOK`.`UTENTI_VISITATORI`(
+        ID char(16) NOT NULL PRIMARY KEY,
+        Nome char(16) NOT NULL ,
+        Cognome char(16) NOT NULL ,
+        Email varchar(20) NOT NULL UNIQUE,
+        Password_acc varchar(20) NOT NULL,
+        Data_di_nascita date,
+        Telefono varchar(10) NOT NULL UNIQUE,
+        Created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        Tipo ENUM('Cliente', 'Amministratore', 'Fornitore')  -- se NULL sono solo utente visitatore (ancora non ho completato la registrazione)
+    );
 
-CREATE TABLE `INBOOK`.`FORNITORI`(
-  	ID_utente_fornitore char(16) NOT NULL PRIMARY KEY,
-    Nome_Attivita varchar(50) NOT NULL,
-    Tipo_Attivita varchar(30) NOT NULL,
-    Indirizzo varchar(60),  --tolto unique
-    Capienza_massima int DEFAULT 1 NOT NULL
-);
+    CREATE TABLE `INBOOK`.`FORNITORI`(
+        ID_utente_fornitore char(16) NOT NULL PRIMARY KEY,
+        Nome_Attivita varchar(50) NOT NULL,
+        Tipo_Attivita varchar(30) NOT NULL,
+        Indirizzo varchar(60),  -- tolto unique
+        Capienza_massima int DEFAULT 1 NOT NULL
+    );
 
-CREATE TABLE `INBOOK`.`ORARI_ATTIVITA`(
-    ID char(16) NOT NULL UNIQUE  PRIMARY KEY,
-    ID_fornitore char(16) NOT NULL,
-    Orario_apertura TIME(0) NOT NULL,   --modificato orario dell'attività(per gestire più orari in base all'attività es. 10:00-13:00 e 16:00-20:00)
-    Orario_chiusura TIME(0) NOT NULL    --hh:mm:ss -> per avere in output hh:mm -> TIME_FORMAT(orario, '%k:%i')
-);
+    CREATE TABLE `INBOOK`.`ORARI_ATTIVITA`(
+        ID char(16) NOT NULL PRIMARY KEY,
+        ID_fornitore char(16) NOT NULL,
+        Orario_apertura TIME(0) NOT NULL,   -- modificato orario dell'attività(per gestire più orari in base all'attività es. 10:00-13:00 e 16:00-20:00)
+        Orario_chiusura TIME(0) NOT NULL    -- hh:mm:ss -> per avere in output hh:mm -> TIME_FORMAT(orario, '%k:%i')
+    );
 
 
-CREATE TABLE `INBOOK`.`SERVIZI`(
-    ID char(16) NOT NULL UNIQUE PRIMARY KEY,
-    ID_fornitore char(16) NOT NULL,
-    Tipologia char(30) NOT NULL,
-    Descrizione char(30),
-    Durata TIME(0) NOT NULL
-);
+    CREATE TABLE `INBOOK`.`SERVIZI`(
+        ID char(16) NOT NULL PRIMARY KEY,
+        ID_fornitore char(16) NOT NULL,
+        Tipologia char(30) NOT NULL,
+        Descrizione char(30),
+        Durata TIME(0) NOT NULL
+    );
 
-CREATE TABLE `INBOOK`.`PRENOTAZIONI`(
-    ID char(16) NOT NULL UNIQUE PRIMARY KEY,
-    ID_utente char(16) NOT NULL ,
-    ID_servizio char(16) NOT NULL ,
-    Orario_richiesta TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    Orario_prenotazione DATETIME NOT NULL,
-    Stato ENUM('Attivo', 'Annullato', 'Completato'),
-    Numero_clienti int DEFAULT 1
-);
+    CREATE TABLE `INBOOK`.`PRENOTAZIONI`(
+        ID char(16) NOT NULL PRIMARY KEY,
+        ID_utente char(16) NOT NULL ,
+        ID_servizio char(16) NOT NULL ,
+        Orario_richiesta TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        Orario_prenotazione DATETIME NOT NULL,
+        Stato ENUM('Attivo', 'Annullato', 'Completato'),
+        Numero_clienti int DEFAULT 1
+    );
 
 
 

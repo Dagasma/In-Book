b7a7cfc86328f3d53a695660b7dad1ade336844b
christianmarescalco <marescalcoc@gmail.com>
2023-01-07 19:58:58 +0100

    Prenotazioni_controller iniziato (da testare)

--- backend/app/controllers/Prenotazioni_controller.js
+++ backend/app/controllers/Prenotazioni_controller.js
@@ -1,9 +1,8 @@
 const db = require("../models");
-const tab_prenotazioni = db.models.PRENOTAZIONI;
+const tab_prenotazioni = db.models.PRENOTAZIONI; //da testare
 const Op = db.Sequelize.Op;
-
-// Create and Save a new User
-exports.create = (req, res) => {
+// Create and Save a new Prenotazione
+exports.effettua_prenotazione = (req, res) => {
     // Validate request
     if (!req.body) {
         res.status(400).send({
@@ -12,18 +11,17 @@ exports.create = (req, res) => {
         return;
     }
 
-    // Create a User
+    // Create a Prenotazione
     const prenotazioni = {
         ID_utente: req.body.ID_utente,
-        ID_prenotazionio: req.body.ID_prenotazionio,
-        Orario_richiesta: req.body.Orario_richiesta,
-        //  insert orario richiesta ?
+        ID_fornitore: req.body.ID_fornitore,
+        ID_servizio: req.body.ID_servizio,
         Orario_prenotazione: req.body.Orario_prenotazione,
         Stato : 'Attivo',
         Numero_clienti: req.body.Numero_clienti,
     };
 
-    // Save User in the database
+    // Save Prenotazione in the database
     tab_prenotazioni.create(prenotazioni)
         .then(data => {
         res.send(data);
@@ -31,116 +29,140 @@ exports.create = (req, res) => {
         .catch(err => {
         res.status(500).send({
             message:
-            err.message || "Some error occurred while creating the User."
+            err.message || "Some error occurred while creating the Prenotazione."
         });
         });
 };
 
-// Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {
-    const title = req.query.title;
-    var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+// Retrieve all Prenotazioni from the database by id utente o id fornitore 
+exports.get_prenotazioni = (req, res) => {
+    var id = null;
+    var condition= null;
 
-    Tutorial.findAll({ where: condition })
+    if(req.query.ID_utente != null){
+      id = req.query.ID_utente;
+      condition = ID_utente ? { ID_utente: { [Op.like]: `%${ID_utente}%` } } : null;
+    }
+    else{
+      id = req.query.ID_fornitore;
+      condition = ID_fornitore ? { ID_fornitore: { [Op.like]: `%${ID_fornitore}%` } } : null;
+    }
+
+    tab_prenotazioni.findAll({ 
+        where: condition,
+        order: [[Orario_prenotazione, 'DESC']]})
         .then(data => {
             res.send(data);
         })
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving tutorials."
+                    err.message || "Some error occurred while retrieving Prenotazioni."
             });
         });
 };
 
-exports.findOne = (req, res) => {
-    const id = req.params.id;
+// Retrieve all slot_liberi from the database by id fornitore  
+exports.get_slot_liberi = (req, res) => {
+  const ID_fornitore = req.query.ID_fornitore;    
+  var condition = ID_fornitore ? { ID_fornitore: { [Op.like]: `%${ID_fornitore}%` } } : null;
+  
+  var filtro = {
+    Stato : 'Attivo',
+    Data_giorno: req.body.Data_giorno
+  }
+  
+  var condition_time = db.sequelize.fn('date', sequelize.col('Orario_prenotazione'), Op.like, filtro.Data_giorno);
 
-    Tutorial.findByPk(id)
-        .then(data => {
-            if (data) {
-                res.send(data);
-            } else {
-                res.status(404).send({
-                    message: `Cannot find Tutorial with id=${id}.`
-                });
-            }
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: "Error retrieving Tutorial with id=" + id
-            });
-        });
+  lista_tab_prenotazioni.findAll({ 
+      where: {
+          condition, 
+          Stato : filtro.Stato,
+          condition_time
+        } })
+      .then(data => {
+          res.send(data);
+      })
+      .catch(err => {
+          res.status(500).send({
+              message:
+                  err.message || "Some error occurred while retrieving Prenotazioni."
+          });
+      });
+  
+  var lista_occupati = data;
+  //completatre
+    
 };
 
 exports.update = (req, res) => {
     const id = req.params.id;
 
-    Tutorial.update(req.body, {
+    tab_prenotazioni.update(req.body, {
         where: { id: id }
     })
         .then(num => {
             if (num == 1) {
                 res.send({
-                    message: "Tutorial was updated successfully."
+                    message: "Prenotazioni was updated successfully."
                 });
             } else {
                 res.send({
-                    message: `Cannot update Tutorial with id=${id}. Maybe Tutorial was not found or req.body is empty!`
+                    message: `Cannot update Prenotazioni with id=${id}. Maybe Prenotazioni was not found or req.body is empty!`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating Tutorial with id=" + id
+                message: "Error updating Prenotazioni with id=" + id
             });
         });
 };
 
 
-// Delete a Tutorial with the specified id in the request
+// Delete a tab_prenotazioni with the specified id in the request
 exports.delete = (req, res) => {
     const id = req.params.id;
   
-    Tutorial.destroy({
+    tab_prenotazioni.destroy({
       where: { id: id }
     })
       .then(num => {
         if (num == 1) {
           res.send({
-            message: "Tutorial was deleted successfully!"
+            message: "Prenotazioni was deleted successfully!"
           });
         } else {
           res.send({
-            message: `Cannot delete Tutorial with id=${id}. Maybe Tutorial was not found!`
+            message: `Cannot delete Prenotazioni with id=${id}. Maybe Prenotazioni was not found!`
           });
         }
       })
       .catch(err => {
         res.status(500).send({
-          message: "Could not delete Tutorial with id=" + id
+          message: "Could not delete Prenotazioni with id=" + id
         });
       });
   };
 
-// Delete all Tutorials from the database.
+// Delete all Prenotazioni from the database.
 exports.deleteAll = (req, res) => {
-    Tutorial.destroy({
+    tab_prenotazioni.destroy({
       where: {},
       truncate: false
     })
       .then(nums => {
-        res.send({ message: `${nums} Tutorials were deleted successfully!` });
+        res.send({ message: `${nums} Prenotazioni were deleted successfully!` });
       })
       .catch(err => {
         res.status(500).send({
           message:
-            err.message || "Some error occurred while removing all tutorials."
+            err.message || "Some error occurred while removing all Prenotazioni."
         });
       });
   };
 
-// Find all published Tutorials
+// Find all published Prenotazioni
 exports.findAllPublished = (req, res) => {
   
 };

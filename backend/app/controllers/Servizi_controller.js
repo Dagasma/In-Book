568702d84523a97aceff8ec26c0e1d1fb25c55d2
christianmarescalco <marescalcoc@gmail.com>
2023-01-07 18:59:47 +0100

    Aggiustato api_routes, Servizi_controller

--- backend/app/controllers/Servizi_controller.js
+++ backend/app/controllers/Servizi_controller.js
@@ -4,8 +4,6 @@ const Op = db.Sequelize.Op;
 
 // Create and Save a new Servizio
 exports.crea_servizio = (req, res) => {
-// Create and Save a new Servizio
-exports.crea_servizio = (req, res) => {
     // Validate request
     if (!req.body) {
         res.status(400).send({
@@ -15,16 +13,13 @@ exports.crea_servizio = (req, res) => {
     }
 
     // Create a Servizio
-    // Create a Servizio
     const servizi = {
-        ID_fornitore: req.kauth.grant.access_token.content.sub,
         ID_fornitore: req.kauth.grant.access_token.content.sub,
         Tipologia: req.body.Tipologia,
         Descrizione: req.body.Descrizione,
         Durata: req.body.Durata,
     };
 
-    // Save Servizio in the database
     // Save Servizio in the database
     tab_servizi.create(servizi)
         .then(data => {
@@ -34,7 +29,6 @@ exports.crea_servizio = (req, res) => {
             res.status(500).send({
                 message:
                     err.message || "Some error occurred while creating the Servizio."
-                    err.message || "Some error occurred while creating the Servizio."
             });
         });
 };
@@ -43,12 +37,7 @@ exports.crea_servizio = (req, res) => {
 exports.get_servizi_per_fornitore = (req, res) => {
     const id_fornitore = req.query.id_fornitore;
     var condition = id_fornitore ? { id_fornitore: { [Op.like]: `%${id_fornitore}%` } } : null;
-// Retrieve all Servizi
-exports.get_servizi_per_fornitore = (req, res) => {
-    const id_fornitore = req.query.id_fornitore;
-    var condition = id_fornitore ? { id_fornitore: { [Op.like]: `%${id_fornitore}%` } } : null;
 
-    tab_servizi.findAll({ where: condition })
     tab_servizi.findAll({ where: condition })
         .then(data => {
             res.send(data);
@@ -56,7 +45,6 @@ exports.get_servizi_per_fornitore = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving Servizi."
                     err.message || "Some error occurred while retrieving Servizi."
             });
         });
@@ -67,25 +55,21 @@ exports.aggiorna_servizio = (req, res) => {
     const id = req.params.id;
 
     tab_servizi.update(req.body, {
-    tab_servizi.update(req.body, {
         where: { id: id }
     })
         .then(num => {
             if (num == 1) {
                 res.send({
                     message: "servizio was updated successfully."
-                    message: "servizio was updated successfully."
                 });
             } else {
                 res.send({
-                    message: `Cannot update servizio with id=${id}. Maybe servizio was not found or req.body is empty!`
                     message: `Cannot update servizio with id=${id}. Maybe servizio was not found or req.body is empty!`
                 });
             }
         })
         .catch(err => {
             res.status(500).send({
-                message: "Error updating servizio with id=" + id
                 message: "Error updating servizio with id=" + id
             });
         });
@@ -132,4 +116,4 @@ exports.delete_all_servizi = (req, res) => {
             err.message || "Some error occurred while removing all servizi."
         });
       });
-  };
+};

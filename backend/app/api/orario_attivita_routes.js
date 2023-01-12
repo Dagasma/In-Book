71af2243d47b1baaae33b0a16502948eda76aaf7
GiulianoDiGiuseppe <digigiu00@gmail.com>
2023-01-12 11:04:05 +0100

    routes

---
+++ backend/app/api/orario_attivita_routes.js
@@ -0,0 +1,17 @@
+const config = require("../../config/config.js");
+
+module.exports = app => {
+    const OrarioAttivita = require("../controllers/OrarioAttivita_controller.js");
+    var router = config.express.Router();
+    
+    // Create a new Fornitore
+    router.post("/", OrarioAttivita.create);
+  
+     //Retrieve a single User by id
+    router.get("/Orario_fornitore/:ID_fornitore", OrarioAttivita.findAll);
+  
+    // Update a User with id
+    router.put("/cambia_ora/:id", OrarioAttivita.update);
+
+    app.use('/OrarioAttivita/api',router);
+   };
---
+++ backend/app/api/votazione_routes.js
@@ -0,0 +1,19 @@
+const config = require("../../config/config.js");
+
+module.exports = app => {
+    const Votazioni = require("../controllers/Votazioni_controller.js");
+    var router = config.express.Router();
+    
+    // Create a new Fornitore
+    router.post("/", Votazioni.create);
+  
+     //Retrieve a single User by id
+    router.get("/findAll_utente/:id", Votazioni.findAll_utente);
+    router.get("/findAll_fornitore/:id", Votazioni.findAll_fornitore);
+
+
+    // Update a User with id
+    router.put("/cambia_voto/:id", Votazioni.update);
+
+    app.use('/Votazioni/api',router);
+   };

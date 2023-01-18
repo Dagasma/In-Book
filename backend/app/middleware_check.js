888625bd792df86c33743b948d06dd204a5301bf
GiulianoDiGiuseppe <digigiu00@gmail.com>
2023-01-18 12:08:12 +0100

    change name href

---
+++ backend/app/middleware_check.js
@@ -0,0 +1,28 @@
+exports.check_id_param = (id_name = "id") => (req,res,next) => {
+
+    if(req.kauth.grant.access_token.content.sub == req.params[id_name]){
+        next();
+    }
+    else{
+        res.status(401).send({
+            message: "Accesso alla risorsa non consentito"
+        });
+    }
+}
+
+exports.check_id_body = (id_name = "id") => (req,res,next) => {
+
+    if(req.kauth.grant.access_token.content.sub == req.body[id_name]){
+        next();
+    }
+    else{
+        res.status(401).send({
+            message: "Accesso alla risorsa non consentito"
+        });
+    }
+}
+
+exports.send_cookie = (req,res,next) => {
+    res.cookie("id",req.kauth.grant.access_token.content.sub);
+    next();
+}

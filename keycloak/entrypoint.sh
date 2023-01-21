c1cac11b515af4aa3cb962a121b291d269ca2bd4
Antonio Avolio <antonioavolio18@gmail.com>
2023-01-21 22:18:19 +0100

    Check dns resolution docker inside ACR
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- keycloak/entrypoint.sh
+++ keycloak/entrypoint.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/wait-for-it.sh db_inbook:3306 -- /opt/keycloak/bin/kc.sh start --optimized --import-realm
+/wait-for-it.sh db_inbook:3306 -t 0 -- /opt/keycloak/bin/kc.sh start --optimized --import-realm --spi-x509cert-lookup-provider=nginx

ec0d086dbc5cf89ffb1197392018e270855a4cf5
Antonio Avolio <antonioavolio18@gmail.com>
2023-02-04 18:08:12 +0100

    Setup vault nodejs
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,3 @@
-
 const config = {};
 require("fix-esm").register();
 config.baseUrl = "https://inbook.azurewebsites.net";
@@ -9,39 +8,70 @@ config.cookieParser = require("cookie-parser");
 config.bodyParser = require("body-parser");
 config.rateLimit = require("express-rate-limit");
 config.Sequelize = require("sequelize");
-config.Keycloak = require('keycloak-connect');
+config.Keycloak = require("keycloak-connect");
 config.fs = require("node:fs");
-config.KcAdminClient= require("@keycloak/keycloak-admin-client").default;
+config.KcAdminClient = require("@keycloak/keycloak-admin-client").default;
 config.kcAdminClient = new config.KcAdminClient();
-config.kcAdminClient.setConfig({ realmName: "inbook",
-                                 baseUrl: config.baseUrl});
+config.kcAdminClient.setConfig({
+    realmName: "inbook",
+    baseUrl: config.baseUrl,
+});
 
-config.keycloak = new config.Keycloak({onLoad: 'login-required', checkLoginIframe: false });
-config.frontend_path = config.path.normalize(process.cwd() + "/frontend/"); 
-config.db_path = config.path.normalize(process.cwd() + "/backend/database/"); 
+config.keycloak = new config.Keycloak({
+    onLoad: "login-required",
+    checkLoginIframe: false,
+});
+config.frontend_path = config.path.normalize(process.cwd() + "/frontend/");
+config.db_path = config.path.normalize(process.cwd() + "/backend/database/");
 
 require("dotenv").config({
-  path: config.path.normalize(process.cwd() + "/backend/config/.env"),
+    path: config.path.normalize("/.env"),
 });
-config.SECRET = process.env.SECRET;
+
+var options = {
+    apiVersion: "v1", // default
+    endpoint: "http://127.0.0.1:8200", // default
+    token: "1234", // optional client token; can be fetched after valid initialization of the server
+};
+
+// get new instance of the client
+var vault = require("node-vault")(options);
+
+vault.approleLogin({
+        role_id: process.env.ROLE_ID,
+        secret_id: process.env.SECRET_ID,
+    })
+    .then((result) => {
+        vault.token = result.auth.client_token;
+        vault.read("kv_inbook/user").then((res) => {
+            console.log(res);
+            config.DB_PASSWORD = res.password;
+            config.DB_USER = res.user;
+        });
+        vault.read("kv_inbook/keycloak").then((res) => {
+            console.log(res);
+            config.SECRET = res.secret;
+        });
+    })
+    .catch(console.error);
+
+
+
 config.DB_HOST = process.env.DB_HOST;
-config.DB_USER = process.env.DB_USER;
-config.DB_PASSWORD = process.env.DB_PASSWORD;
 config.DB_NAME = process.env.DB_NAME;
-config.SECRET = process.env.SECRET;
 config.DB_PORT = process.env.DB_PORT;
-config.PORT = 4000; 
+config.PORT = 4000;
 config.apiLimiter = {
-  windowMs: 60 * 60 * 1000, // 60 minutes
-  max: 5000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    windowMs: 60 * 60 * 1000, // 60 minutes
+    max: 5000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 };
 config.credentials = {
-  grantType: 'client_credentials',
-  clientId:'nodejs',
-  clientSecret: config.SECRET,
-  offlineToken: true
+    grantType: "client_credentials",
+    clientId: "nodejs",
+    clientSecret: config.SECRET,
+    offlineToken: true,
 };
 
 module.exports = config;

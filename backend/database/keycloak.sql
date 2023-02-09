318c5ce9caec39d2e7dcb98a00a594a5e56e7c88
Antonio Avolio <antonioavolio18@gmail.com>
2023-02-09 13:05:10 +0100

    Divided keycloak and backend user
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- backend/database/keycloak.sql
+++ backend/database/keycloak.sql
@@ -1,3 +1,4 @@
+CREATE USER 'administrator_keycloak' IDENTIFIED BY 'dagasma11@';
 CREATE DATABASE `KEYCLOAK`;
-GRANT ALL PRIVILEGES ON `KEYCLOAK`.* TO 'administrator'@'%';
+GRANT ALL PRIVILEGES ON `KEYCLOAK`.* TO 'administrator_keycloak'@'%';
 
